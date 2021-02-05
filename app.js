2fe0247d02a1a2049eb9650415d3a0a8f2395f32
Chase Alber <Calber@students.solano.edu>
2021-02-05 11:23:57 -0800

    initial script done, loading multiple images

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 
 const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 
-
-for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 152; i++) {
     const newImg = document.createElement('img');
-    newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
+    newImg.src = `${baseURL}${i}.png`
     container.appendChild(newImg)
 }

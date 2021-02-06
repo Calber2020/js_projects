25c0acfd2d8e76baf588a79faf51bc01b2f5e02a
Chase Alber <Calber@students.solano.edu>
2021-02-05 20:07:11 -0800

    finished quick script for resuability on deployment of multiple images

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprite
 
 
 for (let i = 1; i < 152; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`
-    container.appendChild(newImg)
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }

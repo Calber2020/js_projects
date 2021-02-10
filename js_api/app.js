9d5b2f975d9e1bf8d3e6b2c5a4743de8053dd692
Chase <calber@students.solano.edu>
2021-02-09 22:15:46 -0800

    "testing again"

--- js_api/app.js
+++ js_api/app.js
@@ -19,18 +19,18 @@
 //     jokes.append(newLI)
 // }
 
-const form = document.querySelector('#searchForm');
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+// const form = document.querySelector('#searchForm');
+// form.addEventListener('submit', async function (e) {
+//     e.preventDefault();
+//     const searchTerm = form.elements.query.value;
+//     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
     
-})
+// })
 
-const makeImage = (shows) => {
-    for(let result of shows) {
-        const img = document.createElement('IMG');
-        img.src = result.show.image.medium;
-        document.body.append(img)
-    }
-}
+// const makeImage = (shows) => {
+//     for(let result of shows) {
+//         const img = document.createElement('IMG');
+//         img.src = result.show.image.medium;
+//         document.body.append(img)
+//     }
+// }

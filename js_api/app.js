29ebcc001183b0c327f28f895df81f4d54e2e972
Chase Alber <Calber@students.solano.edu>
2021-02-09 21:40:49 -0800

    axios function

--- js_api/app.js
+++ js_api/app.js
@@ -1,12 +1,12 @@
 const { default: axios } = require("axios");
 
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-.then(res => {
-    console.log(res.data.ticker.price)
-})
-.catch(err => {
-    console.log("ERROR!", err)
-})
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+// .then(res => {
+//     console.log(res.data.ticker.price)
+// })
+// .catch(err => {
+//     console.log("ERROR!", err)
+// })
 
 const fetchBitcoinPrice = async () => {
     try {
@@ -15,4 +15,9 @@ const fetchBitcoinPrice = async () => {
     }   catch(e) {
         console.log("ERROR!", err)
     }
+}
+
+const getDadJoke = async () => {
+    const res = await axios.get('https://icanhazdadjoke.com/',)
+    console.log(res)
 }

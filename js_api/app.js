97a733eecc5378a1a413c30e7398063c9a2123e7
Chase Alber <Calber@students.solano.edu>
2021-02-09 22:05:45 -0800

    added search with image results

--- js_api/app.js
+++ js_api/app.js
@@ -24,5 +24,7 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    console.log(res.data[0].show.image.medium);
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img)
 })

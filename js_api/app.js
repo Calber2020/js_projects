f11092f226c1b9d4e9fc128d9d4b11537911ffb8
Chase Alber <Calber@students.solano.edu>
2021-02-09 21:52:59 -0800

    setting up new test

--- js_api/app.js
+++ js_api/app.js
@@ -1,18 +1,18 @@
-const { default: axios } = require("axios");
+// const { default: axios } = require("axios");
 
-const fetchBitcoinPrice = async () => {
-    try {
-        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-        console.log(res.data.ticker.price)
-    }   catch(e) {
-        console.log("ERROR!", err)
-    }
-}
-const jokes = document.querySelector('#jokes');
-const getDadJoke = async () => {
-    const config = { headers: { Accept: 'application/json'} }
-    const res = await axios.get('https://icanhazdadjoke.com/', config)
-    const newLI = document.createElement('LI');
-    newLI.append(res.data.joke);
-    jokes.append(newLI)
-}
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price)
+//     }   catch(e) {
+//         console.log("ERROR!", err)
+//     }
+// }
+// const jokes = document.querySelector('#jokes');
+// const getDadJoke = async () => {
+//     const config = { headers: { Accept: 'application/json'} }
+//     const res = await axios.get('https://icanhazdadjoke.com/', config)
+//     const newLI = document.createElement('LI');
+//     newLI.append(res.data.joke);
+//     jokes.append(newLI)
+// }

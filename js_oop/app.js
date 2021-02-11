7929770e43ff61f355ad6194ab704b7648e6ae66
Chase <calber@students.solano.edu>
2021-02-10 22:39:18 -0800

    finished examples for constructor, super, and extend

--- js_oop/app.js
+++ js_oop/app.js
@@ -1,10 +1,18 @@
-class Cat {
+class Pet {
     constructor(name, age){
         this.name = name;
         this.age = age;
     }
     eat() {
         return `${this.name} is eating!`;
+    }
+}
+
+class Cat extends Pet {
+    constructor(name, age, livesLeft = 9) {
+        console.log('IN CAT CONSTRUCTOR!');
+        super(name, age);
+        this.liveLeft = livesLeft;
     }
     meow() {
         return `MEOWWW!!`;
@@ -15,15 +23,8 @@ class Cat {
 // const monty = new Cat('monty', 9)
 // monty.eat()
 
-class Dog {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    eat() {
-        return `${this.name} is eating!`;
-    }
-    bark() {
+class Dog extends Pet {
+   bark() {
         return 'WOOOF!!';
     }
 }

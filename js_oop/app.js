7c92f808a4876f13c5a0c773cd9d4b6dd9deb43a
Chase <calber@students.solano.edu>
2021-02-10 22:18:35 -0800

    added more to the constructor example and fake input

--- js_oop/app.js
+++ js_oop/app.js
@@ -3,4 +3,11 @@ class Cat {
         this.name = name;
         this.age = age;
     }
+    eat() {
+        return `${this.name} is eating!`;
+    }
 }
+
+// EXAMPLE CODE
+// const monty = new Cat('monty', 9)
+// monty.eat

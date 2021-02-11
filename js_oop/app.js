c5ee1477c79ba3ce4a30c7b5e2531cb2f77489e7
Chase <calber@students.solano.edu>
2021-02-10 22:32:29 -0800

    added more examples

--- js_oop/app.js
+++ js_oop/app.js
@@ -6,8 +6,31 @@ class Cat {
     eat() {
         return `${this.name} is eating!`;
     }
+    meow() {
+        return `MEOWWW!!`;
+    }
 }
 
 // EXAMPLE CODE
 // const monty = new Cat('monty', 9)
-// monty.eat
+// monty.eat()
+
+class Dog {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat() {
+        return `${this.name} is eating!`;
+    }
+    bark() {
+        return 'WOOOF!!';
+    }
+}
+
+
+
+
+
+
+
